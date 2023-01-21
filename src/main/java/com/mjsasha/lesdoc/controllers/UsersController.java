4d3d1c8f060d3b2b1b939d568aa41c7e1268cb9e
MJSasha <sasha.ushankov@gmail.com>
2023-01-21 11:59:14 +0300

    Update swagger

--- src/main/java/com/mjsasha/lesdoc/controllers/UsersController.java
+++ src/main/java/com/mjsasha/lesdoc/controllers/UsersController.java
@@ -1,6 +1,5 @@
 package com.mjsasha.lesdoc.controllers;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/test/java/com/mjsasha/lesdoc/UsersControllerTests.java
+++ src/test/java/com/mjsasha/lesdoc/UsersControllerTests.java
@@ -15,7 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-class LesDocApplicationTests {
+class UsersControllerTests {
 
     @Autowired
     private MockMvc mockMvc;
