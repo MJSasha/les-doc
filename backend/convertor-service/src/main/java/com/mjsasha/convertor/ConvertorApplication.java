f86bb4190d9013eb671525b1519130f9a15841a2
MJSasha <sasha.ushankov@gmail.com>
2023-02-27 19:06:37 +0300

    Add convertor-service module

---
+++ backend/convertor-service/src/main/java/com/mjsasha/convertor/ConvertorApplication.java
@@ -0,0 +1,11 @@
+package com.mjsasha.convertor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConvertorApplication.class);
+    }
+}
