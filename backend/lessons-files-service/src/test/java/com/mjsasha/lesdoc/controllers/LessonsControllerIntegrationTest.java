46bb969e3542b7af8e254d2d701bcfd7371de288
MJSasha <sasha.ushankov@gmail.com>
2023-03-11 15:54:52 +0300

    Fix tests

--- backend/lessons-files-service/src/test/java/com/mjsasha/lesdoc/controllers/LessonsControllerIntegrationTest.java
+++ backend/lessons-files-service/src/test/java/com/mjsasha/lesdoc/controllers/LessonsControllerIntegrationTest.java
@@ -11,6 +11,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -32,6 +34,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles(profiles = "test")
+@DirtiesContext
+@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
 class LessonsControllerIntegrationTest {
 
     private final ObjectWriter objectWriter;
