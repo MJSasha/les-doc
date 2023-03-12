e46708139176f66b6ee8c73d7b172819b5482454
MJSasha <sasha.ushankov@gmail.com>
2023-03-12 13:58:05 +0300

    Start adding k8s (kafka not working)

---
+++ k8s/deploy-all.sh
@@ -0,0 +1,5 @@
+kubectl apply -f config.yaml
+kubectl apply -f zookeeper.yaml
+kubectl apply -f kafka.yaml
+kubectl apply -f postgres.yaml
+kubectl apply -f statistics-service.yaml 
---
+++ kind/create-cluster.sh
@@ -0,0 +1,12 @@
+echo "Initializing Kubernetes cluster...\n"
+
+kind delete cluster
+
+kind create cluster --config kind-config.yml
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=90s
